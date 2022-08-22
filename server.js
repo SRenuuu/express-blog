ad1149a98624978627e881c0f3d5b549637cfb45
Nipun Chamikara <nipunchamikara@yahoo.com>
2022-08-23 00:34:37 +0530

    allow app to use port assigned by heroku

--- server.js
+++ server.js
@@ -19,6 +19,6 @@ app.get("/about", (req, res) => {
   res.render("about", { heroTitle: "About" });
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server is running on port 3000");
 });
